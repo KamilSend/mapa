73b626e8315ff668598ecb0bb4ca2e338dd138ea
kamilsendecki <sendej@gmail.com>
2021-03-28 12:53:36 +0200

    Basic RWD

--- mapa/src/components/users/userList/userList.js
+++ mapa/src/components/users/userList/userList.js
@@ -7,7 +7,7 @@ const userList = (props) => {
     const list = props.users.map( (user, index) => {
         return(
             <li key={index}>
-                <span>{index+1}</span>
+                <span className="lp">{index+1}</span>
                 <span>{user.nickname}</span>
                 <span>{user.province}</span>
                 <span>{user.county}</span>
@@ -21,7 +21,7 @@ const userList = (props) => {
 
         <div className="userListWrapper">
             <h2>Lista użytkowników:</h2>
-            <span>Lp</span>
+            <span className="lp">Lp</span>
             <span>Nazwa</span>
             <span>Województwo</span>
             <span>Powiat</span>

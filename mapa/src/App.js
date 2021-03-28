883e9b91e9287514fae173df6791cd6168f65edb
kamilsendecki <sendej@gmail.com>
2021-03-28 12:55:00 +0200

    Remove test buttons

--- mapa/src/App.js
+++ mapa/src/App.js
@@ -26,63 +26,6 @@ class App extends Component {
         loader:false,
     }
 
-    handleTest = () => {
-
-        const data = [
-            {
-                "level": "woj",
-                "v": "Małopolskie"
-            },
-            {
-                "level": "pow",
-                "v": "Kraków"
-            },
-            {
-                "level": "gmi",
-                "v": "Kraków"
-            },
-            {
-                "level": "msc",
-                "v": "Kraków"
-            },
-            {
-                "level": "ulc",
-                "v": "Bociana"
-            },
-            {
-                "level": "kod",
-                "v": "31-231"
-            },
-            {
-                "level": "nr",
-                "v": ""
-            }
-        ]
-
-        const headers = {
-            'Content-Type': 'application/json',
-        }
-
-        axios.post('https://capap.gugik.gov.pl/api/fts/hier/pkt/qq',  data, {
-            headers: headers
-        })
-            .then(res => {
-                // console.log(res);
-                console.log(res.data);
-        })
-            .catch(error => {
-                console.log(error)
-            })
-    }
-
-    firebaseTest = () => {
-        axios.get('https://mapa-6578a-default-rtdb.firebaseio.com/users.json')
-            .then(response => {
-                    console.log(response)
-                }
-            )
-    }
-
     handleFetchUsers = () => {
         this.setState({loader:true})
         const coordinates = []
@@ -126,8 +69,6 @@ class App extends Component {
                         visible={this.state.loader}
                     />
                 </div>:null}
-            <button onClick={this.handleTest}>Test</button>
-            <button onClick={this.firebaseTest}>Test firebase</button>
             <div className="userWrapper">
                 <Signup/>
                 <UserList users={this.state.users}/>
